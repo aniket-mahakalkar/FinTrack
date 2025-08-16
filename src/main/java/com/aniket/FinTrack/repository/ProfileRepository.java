68b20b6c02b1904873eae389dd6ffd407b7b16ef
Aniket Mahakalkar <mahakalkaraniket001@gmail.com>
2025-08-17 00:23:34 +0530

    user registration is done working on email account activation

---
+++ src/main/java/com/aniket/FinTrack/repository/ProfileRepository.java
@@ -0,0 +1,11 @@
+package com.aniket.FinTrack.repository;
+
+import com.aniket.FinTrack.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long>{
+
+    Optional<ProfileEntity> findByEmail (String email);
+}
---
+++ src/main/java/com/aniket/FinTrack/service/ProfileService.java
@@ -0,0 +1,52 @@
+package com.aniket.FinTrack.service;
+
+import com.aniket.FinTrack.dto.ProfileDTO;
+import com.aniket.FinTrack.entity.ProfileEntity;
+import com.aniket.FinTrack.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO){
+
+        ProfileEntity newProfile = toEntity(profileDTO);
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+        String activationLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO){
+
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .password(profileDTO.getPassword())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity){
+
+
+        return  ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
+
+}
